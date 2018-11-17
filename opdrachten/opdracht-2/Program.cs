b0e2a71d7b1f1e4b9dcc988600b4f813f3892f50
Bruno De Coene <brunodecoene@Brunos-MacBook-Pro-2.local>
2018-11-17 17:03:46 +0100

    Revert "Mappenstructuur"
    This reverts commit 353c5a308919a230c55851f3e6b87f264950df83.

--- opdrachten/opdracht-2/Program.cs
+++ opdrachten/opdracht-2/Program.cs
@@ -7,41 +7,33 @@ namespace opdrachten
         static void Main(string[] args)
         {
 
-            // Tafels van vermenigvuldiging
-            Console.WriteLine("- - - - - - - - - - - - - - - - - - - - - -");
-            Console.WriteLine("De tafels van vermenigvuldiging");
-            Console.WriteLine("- - - - - - - - - - - - - - - - - - - - - -");
-            int x = 10;
-                for(int table_1 = 0; table_1 < 10; table_1++) {
-                    Console.WriteLine(table_1 + " x " + x + " = " + (x * table_1));
-            }
+        // Tafels van vermenigvuldiging
 
 
-            // Faculteit
-            Console.WriteLine("- - - - - - - - - - - - - - - - - - - - - -");
-            Console.WriteLine("De faculteit van een getal");
-            Console.WriteLine("- - - - - - - - - - - - - - - - - - - - - -");
-            int faculty(int facultyOf) {
-                return (facultyOf == 1 || facultyOf == 0) ? 1 : faculty(facultyOf - 1) * facultyOf;
-            }
-            Console.WriteLine(faculty(9));
+        // Faculteit
+        int a = 1;
+
+        for( int b = 1; b < 16; b++)
+        {
+            int c = a * b;
+            a = c;
+
+            Console.WriteLine("faculteit van natuurlijk getal is " + a);
+        }
             
+        // Fibonacci
+        int a_fib = 0;
+        int b_fib = 1;
+        int result = 0;
 
-            // Fibonacci
-            Console.WriteLine("- - - - - - - - - - - - - - - - - - - - - -");
-            Console.WriteLine("De reeks van Fibonacci");
-            Console.WriteLine("- - - - - - - - - - - - - - - - - - - - - -");
-            int a_fib = 0;
-            int b_fib = 1;
-            int result = 0;
+        for(int i = 0; i < 10; i++)
+        {
+            result = a_fib + b_fib;
+            Console.WriteLine(result);
 
-            for(int i = 0; i < 20; i++) {
-                result = a_fib + b_fib;
-                Console.WriteLine(result);
-
-                a_fib = b_fib;
-                b_fib = result;
-            }
+            a_fib = b_fib;
+            b_fib = result;
+        }
         }
     }
 }

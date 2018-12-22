bf6180033c3c45ee14df1d8ea19836458564e28a
Bruno De Coene <brunodecoene@Brunos-MacBook-Pro-2.local>
2018-12-22 13:10:57 +0100

    Opdracht 6 deel 3.1

---
+++ opdrachten/opdracht_6/deel_3/3_1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace _3_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose a shape: square, circle, rectangle, parallelogram");
+            string type = Console.ReadLine();
+            Shape obj = CreateShape.getobject(type);
+            Console.WriteLine(obj.Draw());
+            Console.ReadLine();
+        }
+
+        public class CreateShape 
+        {
+            public static Shape getobject(string objshape)
+            {
+                Shape obj = null;
+
+                switch(objshape) {
+                    case "square" : return new Square();
+                    case "circle" : return new Circle();
+                    case "rectangle" : return new Rectangle();
+                    case "parallelogram" : return new Parallelogram();
+                }
+            return obj;
+                
+            }
+        }
+
+        public interface Shape
+        {
+            string Draw();
+        }
+        class Circle: Shape{
+            public virtual string Draw(){
+                return "A circle";
+                
+            }
+        }
+        class Square: Shape{
+            public virtual string Draw(){
+                return "A square";
+            }
+        }
+        class Rectangle: Shape{
+            public virtual string Draw(){
+                return "A rectangle";
+            }
+        }
+        class Parallelogram: Shape{
+            public virtual string Draw(){
+                return "A Parallelogram";
+            }
+        }
+    }
+}
